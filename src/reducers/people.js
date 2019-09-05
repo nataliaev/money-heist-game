bbd9f760edeab20a8040164f5eb42eff0e523454
nataliaev <nataliiaevchenko@gmail.com>
2019-09-05 11:02:40 +0200

    Changed img-team

--- src/reducers/people.js
+++ src/reducers/people.js
@@ -1,6 +1,6 @@
 import { EDIT_PEOPLE, SET_PEOPLE } from '../actions'
 
-export default function people (state = [{name : "Racer", url: "/images/racer.jpg"}, {name: "Hacker", url: "/images/hacker.jpg"}, {name: "Demolition", url: "/images/demolition.jpg"}, {name: "Strongman", url: "/images/strongman.jpg"}, {name: "Counterfeiter", url: "/images/counterfeiter.jpg"}, {name: "Negotiator", url: "/images/negotiator.jpg"}], action = {}) {
+export default function people (state = ["Racer", "Hacker", "Demolition", "Strongman", "Counterfeiter", "Negotiator"], action = {}) {
   switch(action.type) {
     case EDIT_PEOPLE:
       return state.filter(role => role !== action.payload);
