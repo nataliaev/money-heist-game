1130dfa767ff5eb867804f5c849af28ed37d0c68
nataliaev <nataliiaevchenko@gmail.com>
2019-09-05 12:34:32 +0200

    Add stable photos for Team

--- src/components/Team.js
+++ src/components/Team.js
@@ -8,7 +8,7 @@ export default function Team(props) {
       {people.map(person => (
         <div key={person}>
           <img
-            className="people-img"
+            className="people-img-still"
             src="https://res.cloudinary.com/teepublic/image/private/s--GMnUN_vi--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1561482741/production/designs/5160588_0.jpg?fbclid=IwAR2LPb9pCeL7_SmMphztmDeacpoSz69F89M30qQuMgMq-ulkLuQP8SmC2vo"
             alt="team"
           />
